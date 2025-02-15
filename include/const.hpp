e551f8e9edaa3b064fa9442a12bc471fbe0633b8
FelipesCoding <ffcodeyt@gmail.com>
2025-02-15 13:31:48 +0100

    Added more particles to the sim

--- include/const.hpp
+++ include/const.hpp
@@ -4,9 +4,10 @@
 const int WINDOW_WIDTH  = 1280;
 const int WINDOW_HEIGHT = 720;
 
+const int NUM_PARTICLES = 100000;
 const float PARTICLE_RADIUS = 0.5f;
 
-const float DECAY_FACTOR = 0.95f;
+const float DECAY_FACTOR = 0.99f;
 const float DEPOSIT_AMOUNT = 1000.0f; 
 
 #endif // CONST_HPP
